a11ffb927c68a4d59e85763a4b1ea81c0cb0dc05
MyYogurt <40012201+MyYogurt@users.noreply.github.com>
2020-08-10 15:24:58 -0400

    Fixed typo

--- src/pixelator/Launcher.java
+++ src/pixelator/Launcher.java
@@ -3,7 +3,7 @@ package pixelator;
 /**
  * This class is used to launch JARs
  */
-public class Launcer {
+public class Launcher {
     public static void main(String[] args) {
         Main.main(args);
     }
